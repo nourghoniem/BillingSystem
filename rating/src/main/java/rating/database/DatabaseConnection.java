d19f7282e678e61a376ed7a897091f8a6e2f3356
nourghoniem <Nour1704021@miuegypt.edu.eg>
2022-05-09 20:20:48 +0200

    database connection

---
+++ rating/src/main/java/rating/database/DatabaseConnection.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rating.database;
+
+/**
+ *
+ * @author nour
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author nour
+ */
+public class DatabaseConnection {
+
+    private static Connection con;
+
+    public DatabaseConnection() {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Class not found " + e);
+        }
+        try {
+
+            con = DriverManager.getConnection("jdbc:postgresql://bkaxb3w5rvjztkre1shf-postgresql.services.clever-cloud.com:5432/bkaxb3w5rvjztkre1shf", "uiivnrlitesybgtcbqso", "3u6dKo3lOPgPz5gGGWCX");
+            System.out.println("database connected");
+        } catch (Exception ex) {
+    
+            System.out.println("exception at database connection" + ex);
+        }
+    }
+
+    public static Connection getConnection() {
+        return con;
+    }
+
+    public static void closeConnection() {
+        if (con != null) {
+            try {
+                con.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+    }
+
+}
---
+++ rating/src/main/java/rating/database/DatabaseManagement.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rating.database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Time;
+import java.time.ZoneId;
+import java.util.Date;
+
+/**
+ *
+ * @author nour
+ */
+public class DatabaseManagement {
+   
+
+    Connection conn;
+    Statement stmt;
+    ResultSet rs;
+    PreparedStatement pst;
+
+    public DatabaseManagement() {
+        DatabaseConnection c = new DatabaseConnection();
+        conn = c.getConnection();
+        if (conn == null) {
+            System.out.println("database connection is null");
+        }
+    }
+
+}
