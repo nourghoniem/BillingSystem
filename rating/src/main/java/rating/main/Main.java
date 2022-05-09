349210eee417b613a572a4dfe257be0b0ff30e35
nourghoniem <Nour1704021@miuegypt.edu.eg>
2022-05-09 20:47:58 +0200

    database

---
+++ rating/src/main/java/rating/main/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rating.main;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ *
+ * @author nour
+ */
+public class Main {
+    
+        public static void main(final String[] args) throws IOException {
+            
+            
+        }
+    
+}
